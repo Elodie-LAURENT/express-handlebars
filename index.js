c15a9f4e4ebc3fbccf51926c5ebaf3a7f7dddc73
hasina <hasina@MacBook-Pro-de-hasina.local>
2023-07-25 15:32:23 +0300

    Configuring view and Layout Express and Handlebars

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const {engine} = require('express-handlebars');
 
 const PORT = 3000;
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', engine({
+    layoutsDir: __dirname + '/views/layouts',
+}));
+
 
 //static files (import css file)
 app.use(express.static('public'));
 
 app.get('/', (re, res) => {
-    res.send('Hello World!')});
+    // res.send('Hello World!')});
+   res.render('main', {layout: 'index'})
+}); 
 
-app.listen(PORT, ()=> console.log(`App is listenng to port ${PORT}`));
+app.listen(PORT, ()=> console.log(`App is listening to port ${PORT}`));
